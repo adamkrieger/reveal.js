e1af61a888becfb9af9de01cac080f249fe883ad
adamkrieger <adamkrieger.ca@gmail.com>
2017-05-31 14:42:57 -0500

    Updated arch slides. Added a bunch of diagram slides.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 HERE=`pwd`
 
-docker run --name presentation -d -p 7000:80 -p 7001:443 -v $HERE/html:/usr/share/nginx/html akrieger/revealjs
+docker run --name presentation -d -p 7100:80 -p 7101:443 -v $HERE/html:/usr/share/nginx/html akrieger/revealjs
